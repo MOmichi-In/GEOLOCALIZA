885640681a65470b99d46bd58175da3cd36dfcc0
Al-i <aliandresmejia77@gmail.com>
2025-07-01 15:08:38 -0500

    arreglando conflictos

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-<nav x-data="{ open: false }" class="bg-white dark:bg-red-700 border-b border-gray-100 dark:border-red-700">
-=======
 <nav x-data="{ open: false }" class="bg-white bg-red-800 border-gray-100 bg-red-800">
->>>>>>> 64f33851103b05ce73cd65d221518956daa18520
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
